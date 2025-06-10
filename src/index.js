a9599b9a13d9430092ec43c5e22c8dca38001140
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2025-06-10 13:32:53 +0530

    sanskrit support

--- src/index.js
+++ src/index.js
@@ -1,88 +1,268 @@
-function transliterate(text) {
-    const words = text.split(' ');
-    const result = [];
+function transliterate(text, sanskritMode = false) {
+  const words = text.split(" ");
+  const result = [];
 
-    for (const word of words) {
-        const hunterianWord = _devanagariToHunterian(word);
-        result.push(hunterianWord);
-    }
+  for (const word of words) {
+    const hunterianWord = _devanagariToHunterian(word, sanskritMode);
+    result.push(hunterianWord);
+  }
 
-    return result.join(' ');
+  return result.join(" ");
 }
 
-function _devanagariToHunterian(text) {
-    const devanagariMap = {
-        'अ': 'a', 'आ': 'aa', 'इ': 'i', 'ई': 'ii', 'उ': 'u', 'ऊ': 'uu',
-        'ऋ': 'ṛ', 'ॠ': 'ṝ', 'ऌ': 'ḷ', 'ॡ': 'ḹ', 'ए': 'e', 'ऐ': 'ai',
-        'ओ': 'o', 'औ': 'au',
-        'क': 'k', 'ख': 'kh', 'ग': 'g', 'घ': 'gh', 'ङ': 'ṅ',
-        'च': 'ch', 'छ': 'chh', 'ज': 'j', 'झ': 'jh', 'ञ': 'ñ',
-        'ट': 'ṭ', 'ठ': 'ṭh', 'ड': 'ḍ', 'ढ': 'ḍh', 'ण': 'ṇ',
-        'त': 't', 'थ': 'th', 'द': 'd', 'ध': 'dh', 'न': 'n',
-        'प': 'p', 'फ': 'ph', 'ब': 'b', 'भ': 'bh', 'म': 'm',
-        'य': 'y', 'र': 'r', 'ल': 'l', 'व': 'v',
-        'श': 'sh', 'ष': 'sh', 'स': 's', 'ह': 'h',
-        'क्ष': 'ksh', 'त्र': 'tr', 'ज्ञ': 'jñ',
-        'ा': 'aa', 'ि': 'i', 'ी': 'ii', 'ु': 'u', 'ू': 'uu',
-        'ृ': 'ṛ', 'ॄ': 'ṝ', 'ॢ': 'ḷ', 'ॣ': 'ḹ',
-        'े': 'e', 'ै': 'ai', 'ो': 'o', 'ौ': 'au',
-        'ं': 'n', 'ः': 'ḥ', 'ँ': 'n', '्': ''
-    };
+function _devanagariToHunterian(text, sanskritMode) {
+  const casualMap = {
+    अ: "a",
+    आ: "aa",
+    इ: "i",
+    ई: "ii",
+    उ: "u",
+    ऊ: "uu",
+    ऋ: "ṛ",
+    ॠ: "ṝ",
+    ऌ: "ḷ",
+    ॡ: "ḹ",
+    ए: "e",
+    ऐ: "ai",
+    ओ: "o",
+    औ: "au",
+    क: "k",
+    ख: "kh",
+    ग: "g",
+    घ: "gh",
+    ङ: "ṅ",
+    च: "ch",
+    छ: "chh",
+    ज: "j",
+    झ: "jh",
+    ञ: "ñ",
+    ट: "ṭ",
+    ठ: "ṭh",
+    ड: "ḍ",
+    ढ: "ḍh",
+    ण: "ṇ",
+    त: "t",
+    थ: "th",
+    द: "d",
+    ध: "dh",
+    न: "n",
+    प: "p",
+    फ: "ph",
+    ब: "b",
+    भ: "bh",
+    म: "m",
+    य: "y",
+    र: "r",
+    ल: "l",
+    व: "v",
+    श: "sh",
+    ष: "sh",
+    स: "s",
+    ह: "h",
+    क्ष: "ksh",
+    त्र: "tr",
+    ज्ञ: "jñ",
+    "ा": "aa",
+    "ि": "i",
+    "ी": "ii",
+    "ु": "u",
+    "ू": "uu",
+    "ृ": "ṛ",
+    "ॄ": "ṝ",
+    "ॢ": "ḷ",
+    "ॣ": "ḹ",
+    "े": "e",
+    "ै": "ai",
+    "ो": "o",
+    "ौ": "au",
+    "ं": "n",
+    "ः": "ḥ",
+    "ँ": "n",
+    "्": "",
+  };
 
-    let result = '';
-    let i = 0;
+  const sanskritMap = {
+    अ: "a",
+    आ: "ā",
+    इ: "i",
+    ई: "ī",
+    उ: "u",
+    ऊ: "ū",
+    ऋ: "ṛ",
+    ॠ: "ṝ",
+    ऌ: "ḷ",
+    ॡ: "ḹ",
+    ए: "e",
+    ऐ: "ai",
+    ओ: "o",
+    औ: "au",
+    क: "k",
+    ख: "kh",
+    ग: "g",
+    घ: "gh",
+    ङ: "ṅ",
+    च: "ch",
+    छ: "chh",
+    ज: "j",
+    झ: "jh",
+    ञ: "ñ",
+    ट: "ṭ",
+    ठ: "ṭh",
+    ड: "ḍ",
+    ढ: "ḍh",
+    ण: "ṇ",
+    त: "t",
+    थ: "th",
+    द: "d",
+    ध: "dh",
+    न: "n",
+    प: "p",
+    फ: "ph",
+    ब: "b",
+    भ: "bh",
+    म: "m",
+    य: "y",
+    र: "r",
+    ल: "l",
+    व: "v",
+    श: "ś",
+    ष: "ṣ",
+    स: "s",
+    ह: "h",
+    क्ष: "kṣ",
+    त्र: "tr",
+    ज्ञ: "jñ",
+    "ा": "ā",
+    "ि": "i",
+    "ी": "ī",
+    "ु": "u",
+    "ू": "ū",
+    "ृ": "ṛ",
+    "ॄ": "ṝ",
+    "ॢ": "ḷ",
+    "ॣ": "ḹ",
+    "े": "e",
+    "ै": "ai",
+    "ो": "o",
+    "ौ": "au",
+    "ं": "ṁ",
+    "ः": "ḥ",
+    "ँ": "n",
+    "्": "",
+  };
 
-    while (i < text.length) {
-        const currentChar = text[i];
-        let nextChar = text[i + 1] || '';
+  var devanagariMap = sanskritMode ? sanskritMap : casualMap;
 
-        if (i + 1 < text.length && devanagariMap[currentChar + nextChar]) {
-            result += devanagariMap[currentChar + nextChar];
-            i += 2;
-            continue;
-        }
-        
-        if(currentChar === '्') {
-            i++;
-            continue;
-        }
+  let result = "";
+  let i = 0;
 
-        if (devanagariMap[currentChar]) {
-            if (isConsonant(currentChar) && !isVowelSign(nextChar) && nextChar !== '्') {
-                if(nextChar != '' && nextChar != '.' && nextChar != ',' && nextChar != '?') {
-                    result += devanagariMap[currentChar] + 'a';
-                } else {
-                    if(currentChar != "य") {
-                        result += devanagariMap[currentChar];
-                    } else {
-                        result += devanagariMap[currentChar] + 'a';
-                    }
-                }
+  while (i < text.length) {
+    const currentChar = text[i];
+    let nextChar = text[i + 1] || "";
+
+    if (i + 1 < text.length && devanagariMap[currentChar + nextChar]) {
+      result += devanagariMap[currentChar + nextChar];
+      i += 2;
+      continue;
+    }
+
+    if (currentChar === "्") {
+      i++;
+      continue;
+    }
+
+    if (devanagariMap[currentChar]) {
+      if (
+        isConsonant(currentChar) &&
+        !isVowelSign(nextChar) &&
+        nextChar !== "्"
+      ) {
+        if (!sanskritMode) {
+          if (
+            nextChar != "" &&
+            nextChar != "." &&
+            nextChar != "," &&
+            nextChar != "?"
+          ) {
+            result += devanagariMap[currentChar] + "a";
+          } else {
+            if (currentChar != "य") {
+              result += devanagariMap[currentChar];
             } else {
-                result += devanagariMap[currentChar];
+              result += devanagariMap[currentChar] + "a";
             }
+          }
         } else {
-            result += currentChar;
+          result += devanagariMap[currentChar] + "a";
         }
-        i++;
+      } else {
+        result += devanagariMap[currentChar];
+      }
+    } else {
+      result += currentChar;
     }
+    i++;
+  }
 
-    function isConsonant(char) {
-        const consonants = [
-            'क', 'ख', 'ग', 'घ', 'ङ', 'च', 'छ', 'ज', 'झ', 'ञ',
-            'ट', 'ठ', 'ड', 'ढ', 'ण', 'त', 'थ', 'द', 'ध', 'न',
-            'प', 'फ', 'ब', 'भ', 'म', 'य', 'र', 'ल', 'व',
-            'श', 'ष', 'स', 'ह',
-        ];
-        return consonants.includes(char);
-    }
+  function isConsonant(char) {
+    const consonants = [
+      "क",
+      "ख",
+      "ग",
+      "घ",
+      "ङ",
+      "च",
+      "छ",
+      "ज",
+      "झ",
+      "ञ",
+      "ट",
+      "ठ",
+      "ड",
+      "ढ",
+      "ण",
+      "त",
+      "थ",
+      "द",
+      "ध",
+      "न",
+      "प",
+      "फ",
+      "ब",
+      "भ",
+      "म",
+      "य",
+      "र",
+      "ल",
+      "व",
+      "श",
+      "ष",
+      "स",
+      "ह",
+    ];
+    return consonants.includes(char);
+  }
 
-    function isVowelSign(char) {
-        const vowelSigns = ['ा', 'ि', 'ी', 'ु', 'ू', 'ृ', 'ॄ', 'ॢ', 'ॣ', 'े', 'ै', 'ो', 'ौ'];
-        return vowelSigns.includes(char);
-    }
+  function isVowelSign(char) {
+    const vowelSigns = [
+      "ा",
+      "ि",
+      "ी",
+      "ु",
+      "ू",
+      "ृ",
+      "ॄ",
+      "ॢ",
+      "ॣ",
+      "े",
+      "ै",
+      "ो",
+      "ौ",
+    ];
+    return vowelSigns.includes(char);
+  }
 
-    return result;
+  return result;
 }
 
 module.exports = { transliterate };
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const { transliterate } = require('./src/index.js');
 
-const tests = [
+const testsCasual = [
     "नमस्ते",
     "क्षत्रिय",
     "भारत",
@@ -14,6 +14,16 @@ const tests = [
     "कविताएं अक्सर भावनाओं जैसे प्रेम, दुख, खुशी और निराशा को व्यक्त करती हैं"
 ];
 
-tests.forEach(test => {
-    console.log(`Devanagari: ${test} \nRoman: ${transliterate(test)} \n---`);
+const testsSanskrit = [
+    "विद्या विनति सर्वं विश्वे, सूर्यस्य तेजसि संनादति",
+    "नित्यं शान्तिमयी च रम्या, तया जीवनं समृद्धं भवति",
+    "हृदये तस्याः संनादति सौम्यं, बुद्ध्या विश्वं प्रकाशति"
+];
+
+testsCasual.forEach(test => {
+    console.log(`Devanagari: ${test} \nRomanized: ${transliterate(test)} \n---`);
+});
+
+testsSanskrit.forEach(test => {
+    console.log(`Devanagari Sanskrit: ${test} \nRomanized: ${transliterate(test, true)} \n---`);
 });
